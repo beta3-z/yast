91165bc41b7611423208dcaf3db9dde7b331d9ab
MaximEmelyanchenko <beta3-z@mail.ru>
2018-11-18 16:41:04 +0300

    add postinstall task with electron-rebuild

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
 import {app, BrowserWindow} from 'electron';
 import * as path from 'path';
+import {getConnection} from 'oracledb';
 
-function createWindow() : void {
-    const indexPath: string = path.join(path.resolve('./'), 'index.html');
-    const window: BrowserWindow = new BrowserWindow({
+function createWindow() {
+    const indexPath = path.join(path.resolve('./'), 'index.html');
+    const window = new BrowserWindow({
         width: 1024,
         height: 768
     });
 
+    console.log(process.env);
+
     console.log(indexPath);
 
     window.loadFile(indexPath);
+
+    getConnection(
+        {
+            user: 'SIEBEL',
+            password: 'SIEBEL',
+            connectString: 'LOCAL_XE'
+        },
+        (err, connection) => {
+            if(!err) connection.close();
+
+            console.log('hello there');
+        }
+    );
 }
 
 app.on('ready', createWindow);
